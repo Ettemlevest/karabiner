3c2e06d56c8227e99983ad1ecea4b9f24d345017
Imre Nagy <i.nagy23@outlook.com>
2024-12-04 11:40:22 +0100

    reorganize and drop unused rules

--- rules.ts
+++ rules.ts
@@ -42,6 +42,8 @@ const rules: KarabinerRules[] = [
     ],
   },
   ...createHyperSubLayers({
+    equal_sign: app("Kitty"),
+
     o: { // *O*pen applications
       1: app("1Password"),
       a: app("Arc"),
@@ -53,7 +55,6 @@ const rules: KarabinerRules[] = [
       p: app("PHPStorm"),
       s: app("Slack"),
       t: app("TickTick"),
-      v: app("Visual Studio Code")
     },
 
     // TODO: This doesn't quite work yet.
@@ -72,29 +73,21 @@ const rules: KarabinerRules[] = [
     // },
 
     w: { // *W*indow management via rectangle.app
+      // positioning
       h: rectangle("first-three-fourths"),
       j: rectangle("first-two-thirds"),
       k: rectangle("center"),
       l: rectangle("last-third"),
       semicolon: rectangle("last-fourth"),
-      f: rectangle("maximize"),
-      return_or_enter: rectangle("maximize"),
-      hyphen: rectangle("smaller"),
-      equal_sign: rectangle("larger"),
-      left_arrow: rectangle("previous-display"),
-      right_arrow: rectangle("next-display"),
       m: rectangle("left-half"),
       comma: rectangle("right-half"),
-
-      n: {
-        description: "Window: Next Window",
-        to: [
-          {
-            key_code: "grave_accent_and_tilde",
-            modifiers: ["right_command"],
-          },
-        ],
-      },
+      // sizing
+      f: rectangle("maximize"),
+      hyphen: rectangle("smaller"), // ü
+      equal_sign: rectangle("larger"), // ó
+      // displays
+      left_arrow: rectangle("previous-display"),
+      right_arrow: rectangle("next-display"),
     },
 
     s: { // *S*ystem
@@ -108,11 +101,10 @@ const rules: KarabinerRules[] = [
 
     // TODO: find a better shortcut key, using R here is not ideal
     r: { // *R*aycast
-      a: open("raycast://extensions/raycast/raycast-ai/ai-chat"),
-      c: open("raycast://extensions/thomas/color-picker/pick-color"),
-      e: open("raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"),
+      j: open("raycast://extensions/raycast/emoji-symbols/search-emoji-symbols"),
       h: open("raycast://extensions/raycast/calculator/calculator-history"),
       l: open("raycast://extensions/the-browser-company/arc/search-tabs"),
+      m: open("raycast://extensions/raycast/navigation/search-menu-items"),
       n: open("raycast://extensions/raycast/snippets/search-snippets"),
       p: open("raycast://extensions/raycast/raycast/confetti"),
       s: open("raycast://extensions/raycast/screenshots/search-screenshots"),
